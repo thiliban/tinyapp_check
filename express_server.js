3efee7a8ad5ca5592bfb6b774dd869ddef72c6c9
Thiliban Baskaran <thiliban.b@gmail.com>
2023-08-20 19:18:30 +0000

    Express Server

--- express_server.js
+++ express_server.js
@@ -17,6 +17,16 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+    const templateVars = {urls: urlDatabase};
+    res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: req.params.longURL};
+    res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
